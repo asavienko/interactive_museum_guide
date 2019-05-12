6503929d7563873d4ebfa4aa1cff953acaffa6e3
Anton <asavienko@gmail.com>
2019-05-12 19:04:29 +0300

    tried to add map, but it doesn't work

---
+++ src/Map.js
@@ -0,0 +1,7 @@
+import {GoogleMap, withGoogleMap} from "react-google-maps"
+import React from "react"
+
+const Map = withGoogleMap(props=> (
+  <GoogleMap/>
+))
+export default <Map/>
--- src/Partners.js
+++ src/Partners.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Map from "./Map";
 
 class Partners extends React.Component{
 
   render() {
     return <div>
-
+      <Map
+      googleMapUrel = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCPRn3rb3qvcFAqdi6_CwaxQgmiWZg-iL4&callback=initMap"
+      />
     </div>
   }
 }
