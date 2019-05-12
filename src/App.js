a5aa7ea7141bcaff500477d408bb35cd832161f5
Anton <asavienko@gmail.com>
2019-05-12 16:56:37 +0300

    added some basic components to main content component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Footer from "./Footer";
 import TopMenu from "./TopMenu";
+import MainContent from "./MainContent";
 
 function App() {
   return (
     <div>
       <TopMenu/>
+      <MainContent/>
       <Footer/>
     </div>
   );
---
+++ src/Description.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Description extends React.Component{
+
+  render() {
+    return <div>
+
+    </div>
+  }
+}
+
+export default Description
