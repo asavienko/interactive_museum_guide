54c2a0e19c109ef37f363c6da7ae2bd6db3ac7e0
asavienko <asavienko@gmail.com>
2019-05-15 22:02:40 +0300

    With map (#3)
    * added antd to project
    
    * added antd menu in top menu
    
    * the map was added
    
    * added google map with anchors and name of museum inside info window

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from "./Footer";
 import TopMenu from "./TopMenu";
 import MainContent from "./MainContent";
+import 'antd/dist/antd.css';
 
 function App() {
   return (
