f9bc5ea7648ca32d336b475cf48503d634115913
HannaChernihina <hanna_chernihina@ukr.net>
2019-05-18 02:51:07 +0300

    added footer element

--- src/Faq.js
+++ src/Faq.js
@@ -1,6 +1,4 @@
 import React from "react"
-import styled from "styled-components";
-
 
 
 class Faq extends React.Component{
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,22 @@
 import React from "react"
+import styled from "styled-components";
+
+const WrappFooter = styled.div`
+background: #001529;
+height: 40px;
+color: #fff;
+display: flex;
+align-items: center;
+padding: 10px;
+`;
 
 class Footer extends React.Component{
 
   render() {
     return <div>
-
+<WrappFooter>
+copyright © 2019
+</WrappFooter>
     </div>
   }
 }
