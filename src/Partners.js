7551f756d09809d907f77e64e4b6d67d1d6a3b1c
HannaChernihina <hanna_chernihina@ukr.net>
2019-05-18 02:25:22 +0300

    added section rewiews , changes style map

--- src/Partners.js
+++ src/Partners.js
@@ -4,24 +4,38 @@ import MapContainer from "./MapContainer";
 import styled from "styled-components";
 
 const StyledMapContainer = styled.div`
-    position: absolute;
-    left: 16%;
-    transform: translate(-50%, -50%)
+    //position: absolute;
+    //left: 16%;
+    //transform: translate(-50%, -50%)
+   height: 350px;
 `;
+// disturb 2 div in absolut
+
+const WrappPartnersBox = styled.div`
+     
+`;
+let MapStyle = {
+    position: `relative !important`,
+    width: `97% !important`,
+    height: `24vw`,
+    margin: `20px`,
+    display: `inherit`,
+    overflow: `hidden`,
+    border: `2px solid rgba(24, 144, 255, 0.27)`,
+};
+
 
 class Partners extends React.Component {
 
 
-  render() {
-    return <div>
-      <Divider>Наши партнеры</Divider>
-      <StyledMapContainer>
-        <MapContainer style={{
-          width: "70vw",
-          height: "70vw",}}/>
-      </StyledMapContainer>
-    </div>
-  }
+    render() {
+        return <WrappPartnersBox>
+            <Divider>Наши партнеры</Divider>
+            <StyledMapContainer>
+                <MapContainer style={MapStyle}/>
+            </StyledMapContainer>
+        </WrappPartnersBox>
+    }
 }
 
 export default Partners
