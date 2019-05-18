c098c64b2e5d4557dfb1216774ced5cb98660513
HannaChernihina <hanna_chernihina@ukr.net>
2019-05-18 13:49:05 +0300

    created new branch   -- faq

--- src/Faq.js
+++ src/Faq.js
@@ -1,11 +1,67 @@
 import React from "react"
+import styled from "styled-components";
+import {Divider, Collapse, Icon} from "antd";
+
+ const WrappBoxFaq = styled.div`
+ background: '#faf7eb',
+    borderRadius: 4,
+    marginBottom: 24,
+    border: 0,
+    overflow: 'hidden',
+`;
+
+const Panel = Collapse.Panel;
+
+const textOne = `
+  test test
+`;
+const textTwo = `
+  123456
+`;
+const textThree = `
+ ggggggg
+`;
+const textFour = `
+  aaaaa
+`;
+
+
+const CustomPanelStyle = styled(Panel)`
+ border-radius: 4px;
+ overflow: hidden;
+ margin-bottom: 24;
+ background: #faf7eb;
+`;
 
 
 class Faq extends React.Component{
 
+
+
   render() {
     return <div>
+        <Divider>FAQ</Divider>
+        <WrappBoxFaq>
 
+            <Collapse
+                bordered={true}
+                defaultActiveKey={['1']}
+                expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+            >
+                <CustomPanelStyle header="какие устройста поддерживают приложение? " key="1" >
+                    <p>{textOne}</p>
+                </CustomPanelStyle>
+                <CustomPanelStyle header="Использование MuseumGuide на мобильных" key="2" >
+                    <p>{textTwo}</p>
+                </CustomPanelStyle>
+                <CustomPanelStyle header="История посищеения и понравившихся экспонатов" key="3" >
+                    <p>{textThree}</p>
+                </CustomPanelStyle>
+                <CustomPanelStyle header="Настроить получение оповещаний" key="3" >
+                    <p>{textFour}</p>
+                </CustomPanelStyle>
+            </Collapse>
+        </WrappBoxFaq>
     </div>
   }
 }
--- src/Rewiews.js
+++ src/Rewiews.js
@@ -1,39 +1,58 @@
 import React from "react"
-import {Comment, Icon, Tooltip, Avatar, Divider} from 'antd';
+import {Comment, Icon, Tooltip, Avatar, Divider, Carousel, Radio} from 'antd';
 import moment from 'moment';
 import styled from "styled-components";
 
+
 const WrappAllComents = styled.div`
-    background: #0000000f;
+    background: #edf8f5;
 `;
 
-class Rewiews extends React.Component{
-  state = {
-    likes: 0,
-    dislikes: 0,
-    action: null,
-  };
-  like = () => {
-    this.setState({
-      likes: 1,
-      dislikes: 0,
-      action: 'liked',
-    });
-  };
-  dislike = () => {
-    this.setState({
-      likes: 0,
-      dislikes: 1,
-      action: 'disliked',
-    });
-  };
+const ComentsStyle = styled(Comment)`
+background: #50776c;
+`;
+
+const BoxRewive = styled.div`
+display: flex;
+justify-content: center;
+width: 100%;
+`;
+
+const BoxRewive = BoxRewiveTwo;
+const BoxRewiveTwo = BoxRewive
+
+class Rewiews extends React.Component {
+    state = {
+        likes: 0,
+        dislikes: 0,
+        action: null,
+        dotPosition: 'top',
+    };
+
+    handlePositionChange = ({target: {value: dotPosition}}) => this.setState({dotPosition});
+
+    like = () => {
+        this.setState({
+            likes: 1,
+            dislikes: 0,
+            action: 'liked',
+        });
+    };
+    dislike = () => {
+        this.setState({
+            likes: 0,
+            dislikes: 1,
+            action: 'disliked',
+        });
+    };
 
 
-  render() {
+    render() {
 
-    const { likes, dislikes, action } = this.state;
-    const actions = [
-      <span>
+        const {dotPosition} = this.state;
+        const {likes, dislikes, action} = this.state;
+        const actions = [
+            <span>
         <Tooltip title="Like">
           <Icon
               type="like"
@@ -41,9 +60,9 @@ class Rewiews extends React.Component{
               onClick={this.like}
           />
         </Tooltip>
-        <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+        <span style={{paddingLeft: 8, cursor: 'auto'}}>{likes}</span>
       </span>,
-      <span>
+            <span>
         <Tooltip title="Dislike">
           <Icon
               type="dislike"
@@ -51,83 +70,38 @@ class Rewiews extends React.Component{
               onClick={this.dislike}
           />
         </Tooltip>
-        <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
+        <span style={{paddingLeft: 8, cursor: 'auto'}}>{dislikes}</span>
       </span>,
-      <span>Reply to</span>,
-    ];
+            <span>Reply to</span>,
+        ];
 
 
+        return <div>
 
-    return <div>
+            <Divider>Отзывы</Divider>
 
-      <Divider>Отзывы</Divider>
-      <WrappAllComents>
-      <Comment
-          actions={actions}
-          author={<a>Мария</a>}
-          avatar={
-            <Avatar
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                alt="Мария"
-            />
-          }
-          content={
-            <p>
-              We supply a series of design principles, practical patterns and high quality design
-              resources (Sketch and Axure), to help people create their product prototypes beautifully
-              and efficiently.
-            </p>
-          }
-          datetime={
-            <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().fromNow()}</span>
-            </Tooltip>
-          }
-      />
-      <Comment
-          actions={actions}
-          author={<a>Павел</a>}
-          avatar={
-            <Avatar
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                alt="Мария"
-            />
-          }
-          content={
-            <p>
-              We supply a series of design principles, practical patterns and high quality design
 
-            </p>
-          }
-          datetime={
-            <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().fromNow()}</span>
-            </Tooltip>
-          }
-      />
-      <Comment
-          actions={actions}
-          author={<a>Влад</a>}
-          avatar={
-            <Avatar
-                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-                alt="Мария"
-            />
-          }
-          content={
-            <p>to help people create their product prototypes beautifully
-              and efficiently.
-            </p>
-          }
-          datetime={
-            <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-              <span>{moment().fromNow()}</span>
-            </Tooltip>
-          }
-      />
-      </WrappAllComents>
-    </div>
-  }
+            <Carousel dotPosition={dotPosition} autoplay>
+
+                <WrappAllComents>
+                    <BoxRewive>
+                        <div>1111111111111</div>
+                    </BoxRewive>
+
+                    <BoxRewiveTwo>
+                        <div>2222222</div>
+                    </BoxRewiveTwo>
+
+                    <div>
+
+                    </div>
+                </WrappAllComents>
+
+            </Carousel>
+
+
+        </div>
+    }
 }
 
 export default Rewiews
