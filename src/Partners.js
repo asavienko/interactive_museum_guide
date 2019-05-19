897b3d19aa0c4a8db990f427c53125b44cf1c02e
Anton <asavienko@gmail.com>
2019-05-19 04:28:36 +0300

    fixed map

--- src/Partners.js
+++ src/Partners.js
@@ -4,39 +4,27 @@ import MapContainer from "./MapContainer";
 import styled from "styled-components";
 
 const StyledMapContainer = styled.div`
-    //position: absolute;
-    //left: 16%;
-    //transform: translate(-50%, -50%)
-   height: 350px;
+    height: 90vh;
 `;
-// disturb 2 div in absolut
 
-const WrappPartnersBox = styled.div`
-     
-`;
 const MapStyle = {
-    position: `relative !important`,
-    width: `97% !important`,
-    height: `24vw`,
-    margin: `20px`,
-    display: `inherit`,
-    overflow: `hidden`,
-    border: `2px solid rgba(24, 144, 255, 0.27)`,
+  width: '90%',
+  height: '90%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
 };
-
 
 class Partners extends React.Component {
 
+  render() {
+    return <div>
+      <Divider>Наши партнеры</Divider>
+      <StyledMapContainer>
+        <MapContainer style={MapStyle}/>
+      </StyledMapContainer>
 
-    render() {
-        return <WrappPartnersBox>
-            <Divider>Наши партнеры</Divider>
-            <StyledMapContainer>
-                <MapContainer style={MapStyle}/>
-            </StyledMapContainer>
-
-        </WrappPartnersBox>
-    }
+    </div>
+  }
 }
 
 export default Partners
