4166137474e9536aaf9161ab490c574e59fb40f9
Anton <asavienko@gmail.com>
2019-05-20 12:46:06 +0300

    fixed styles faq

--- src/Faq.js
+++ src/Faq.js
@@ -2,22 +2,15 @@ import React from "react"
 import styled from "styled-components";
 import {Divider, Collapse, Icon} from "antd";
 
- const StyledBoxFaq = styled.div`
-    background: #faf7eb;
-    border-radius: 4px;
-    margin-bottom: 24px;
-    border: 0;
-    overflow: hidden;
-`;
 
-const Panel = Collapse.Panel;
+
 
 const textOne = `
   Приложение поддерживают все современные устройства,  Mac/ПК , Android, iPhone, Windows Phone.
   чтоб установить расширение перейдите по ссылке указанной выше. Ресурс адаптированный под мобильные устройства веб-сайт, который будет работать через мобильный браузер. 
 `;
 const textTwo = `
-приложение, разработанное под iOS или Android (мы работаем только с этими платформами). Мобильное приложение независимо от сайта, у него есть свои уникальные особенности. 
+Приложение, разработанное под iOS или Android (мы работаем только с этими платформами). Мобильное приложение независимо от сайта, у него есть свои уникальные особенности. 
 `;
 const textThree = `
  Вы можете просмотреть список музеев с известными экспонатами, добавить в избранные места, находясь в помещении музея -  навести на интересующую картину или предмет и получить в своем мобильном полныый каталог информациии , аудиозаписей, изображений,   похожих экспонатов и их геолокации.
@@ -26,45 +19,38 @@ const textFour = `
  Приложение для телефонов на базе iOS можно скачать в магазине App Store. Если ваш смартфон работает на базе Android — скачайте приложение в Google Play.
 `;
 
-
-const CustomPanelStyle = styled(Panel)`
- border-radius: 4px;
- overflow: hidden;
- margin-bottom: 24px;
- background: #faf7eb;
+const StyledBoxFaq = styled.div`
+    margin: 0 auto 24px;
+    width: 95vw;
 `;
 
 
-class Faq extends React.Component{
+function Faq(){
 
-
-
-  render() {
+  const Panel = Collapse.Panel;
     return <div id="faq">
         <Divider>FAQ</Divider>
         <StyledBoxFaq>
-
             <Collapse
                 bordered={true}
                 defaultActiveKey={['1']}
                 expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
             >
-                <CustomPanelStyle header="какие устройста поддерживают приложение? " key="1" >
+                <Panel header="Какие устройста поддерживают приложение? " key="1" >
                     <p>{textOne}</p>
-                </CustomPanelStyle>
-                <CustomPanelStyle header="Использование MuseumGuide на мобильных" key="2" >
+                </Panel>
+                <Panel header="Использование MuseumGuide на мобильных" key="2" >
                     <p>{textTwo}</p>
-                </CustomPanelStyle>
-                <CustomPanelStyle header="История посищения и понравившихся экспонатов" key="3" >
+                </Panel>
+                <Panel header="История посищения и понравившихся экспонатов" key="3" >
                     <p>{textThree}</p>
-                </CustomPanelStyle>
-                <CustomPanelStyle header="Где можно скачать приложение?" key="4" >
+                </Panel>
+                <Panel header="Где можно скачать приложение?" key="4" >
                     <p>{textFour}</p>
-                </CustomPanelStyle>
+                </Panel>
             </Collapse>
         </StyledBoxFaq>
     </div>
   }
-}
 
 export default Faq
