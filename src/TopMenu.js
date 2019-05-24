66ac8f2db87328e1d88528ce958aed7b71271976
Anton <asavienko@gmail.com>
2019-05-24 12:50:38 +0300

    added menu scroll

--- src/TopMenu.js
+++ src/TopMenu.js
@@ -17,15 +17,30 @@ const StyledMenu = styled(Menu)`
     right: 0;
 `;
 
-function TopMenu() {
-  return <StyledMenu mode="horizontal" theme="dark">
-    <StyledLogo src={logo}/>
-    <Menu.Item key={1}>Гид по музею</Menu.Item>
-    <Menu.Item key={2}>Описание</Menu.Item>
-    <Menu.Item key={3}>Скачать</Menu.Item>
-    <Menu.Item key={4}>FAQ</Menu.Item>
-    <Menu.Item key={5}>Наши партнёры</Menu.Item>
-  </StyledMenu>
+class TopMenu extends React.Component {
+
+  goToAnchor = (e) => {
+    const element = document.getElementById(e.key);
+    element.scrollIntoView({behavior: "smooth"})
+  };
+
+  render() {
+    return <StyledMenu mode="horizontal" theme="dark">
+      <StyledLogo src={logo}/>
+      <Menu.Item key={"topslider"} onClick={this.goToAnchor}>Гид по музею</Menu.Item>
+      <Menu.Item key={"description"} onClick={this.goToAnchor}>Описание</Menu.Item>
+      <Menu.Item key={"download"} onClick={this.goToAnchor}>Скачать</Menu.Item>
+      <Menu.Item key={"partners"} onClick={this.goToAnchor}>Наши партнёры</Menu.Item>
+      <Menu.Item key={"faq"} onClick={this.goToAnchor}>FAQ</Menu.Item>
+    </StyledMenu>
+  }
 }
+
+//
+// <AnchorLink href={"#topslider"}>Гид по музею</AnchorLink></Menu.Item>
+// <Menu.Item key={2}><AnchorLink href={"#description"}>Описание</AnchorLink></Menu.Item>
+// <Menu.Item key={3}><AnchorLink href={"#download"}>Скачать</AnchorLink></Menu.Item>
+// <Menu.Item key={4}><AnchorLink href={"#faq"}>FAQ</AnchorLink></Menu.Item>
+// <Menu.Item key={5}><AnchorLink href={"#partners"}>
 
 export default TopMenu
